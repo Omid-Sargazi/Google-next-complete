8fa46a5e9748133a0c56f71910ce320615116ef8
OmidSargazi <om.sargazi@gmail.com>
2024-03-20 06:36:16 +0330

    add footer component

---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,19 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+export default function CountryLookup() {
+    const [country, setCountry] = useState("United State")
+    useEffect(()=>{
+        const getCountry = async()=>{
+        const response = await fetch("https://extreme-ip-lookup.com/json/")
+        .then(res=>res.json())
+        .then(data=>data.country)
+        if(!response) return;
+        setCountry(response)
+        } 
+            getCountry()
+    },[])
+  return (
+    <div>USA</div>
+  )
+}
